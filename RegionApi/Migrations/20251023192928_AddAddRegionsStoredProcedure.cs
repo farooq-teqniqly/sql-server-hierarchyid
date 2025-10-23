b8290b260c6ca52eced52e7f1e02b79eb20a6b76
teqniqly <teqniqly@teqniqly.com>
2025-10-23 12:38:23 -0700

    Add stored procedure for adding regions and related migration files

---
+++ RegionApi/Migrations/20251023192928_AddAddRegionsStoredProcedure.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using RegionApi.Utilities;
+
+#nullable disable
+
+namespace RegionApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAddRegionsStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = SqlResourceHelper.ReadEmbeddedSql("RegionApi.Sql.Add_Region.sql");
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"IF OBJECT_ID(N'dbo.Region_Add', N'P') IS NOT NULL DROP PROCEDURE dbo.Region_Add;"
+            );
+        }
+    }
+}
---
+++ RegionApi/Utilities/SqlResourceHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace RegionApi.Utilities
+{
+    public static class SqlResourceHelper
+    {
+        public static string ReadEmbeddedSql(string resourceName)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(resourceName);
+
+            var assembly = Assembly.GetExecutingAssembly();
+            using var stream = assembly.GetManifestResourceStream(resourceName)!;
+            using var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+}
